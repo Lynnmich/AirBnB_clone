93a5fd830222063d64713f4d76376fc19db0f7ac
Afis <mahoutinafis@gmail.com>
2023-03-11 00:08:02 +0100

    update base_model

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
 import copy
 from datetime import datetime
 import uuid
+from models import storage
 
 
 class BaseModel:
@@ -27,6 +28,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = copy.deepcopy(self.created_at)
+        storage.new(self)
 
     def __str__(self):
         """String representation of Base Model"""
