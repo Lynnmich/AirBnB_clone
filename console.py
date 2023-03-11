fc1bc493dbb31736ec7f0f666fc2f2d0a879aa23
Afis <mahoutinafis@gmail.com>
2023-03-11 01:05:32 +0100

    implement arg checker

--- console.py
+++ console.py
@@ -4,6 +4,8 @@ This contains th Entry point of the command interpreter
 """
 import cmd
 import sys
+import models
+
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -26,6 +28,38 @@ class HBNBCommand(cmd.Cmd):
         If empty line
         """
         return
+
+    def check_args(self, nb_args, check_clsname=True,
+                                 check_id=False):
+        """Check if passed args repect some
+           Criteria
+        """
+
+        if (check_clsname):
+            if not len(nb_args):
+                print("** class name missing **")
+                return 0
+
+            try:
+                base_ = eval(nb_args[0])()
+                id = "{}.{}".format(nb_args[0], base_.id)
+                del models.storage.all()[id]
+                models.storage.save()
+            except NameError:
+                print("** class doesn't exist **")
+                return 0
+
+        if check_id:
+            if len(nb_args) < 2:
+                print("** instance id missing **")
+                return 0
+
+            id = "{}.{}".format(nb_args[0], nb_args[1])
+            if (id not in models.storage.all().keys()):
+                print("** no instance found **")
+                return 0
+
+        return 1 
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
